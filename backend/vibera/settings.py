5eb643087ced4c9761404232f058bc8b4f349a4a
Mujahid Islam <mr.mujahidislam1080@gmail.com>
2025-12-29 15:08:05 +0500

    chore(auth): remove JWT token blacklisting

--- backend/vibera/settings.py
+++ backend/vibera/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
-    "rest_framework_simplejwt.token_blacklist",
     "rest_framework.authtoken",
     "djoser",
     "users",
@@ -144,8 +143,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": True,
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,
